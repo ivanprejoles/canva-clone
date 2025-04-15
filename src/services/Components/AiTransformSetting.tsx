91f8df2400d6cd5037e1bc231d554228d1040f57
ivanprejoles <ivanpatrickprejoles@gmail.com>
2025-04-15 17:59:05 +0800

    added

--- src/services/Components/AiTransformSetting.tsx
+++ src/services/Components/AiTransformSetting.tsx
@@ -15,15 +15,21 @@ const AiTransformSetting = () => {
       </h2>
       <div className="mt-3 grid grid-cols-2 gap-3 overflow-auto p-1 custom-scrollbar">
         {AITransformationSettings.map((option, index) => (
-          <div className="" key={index} onClick={() => setSelectedAi(option)}>
+          <div
+            className="bg-white/5 p-2 rounded-xl cursor-pointer transition-all duration-200 hover:bg-white/10 hover:shadow-md hover:translate-y-[-2px] active:translate-y-[0px] active:bg-white/15 border border-transparent hover:border-white/10"
+            key={index}
+            onClick={() => setSelectedAi(option)}
+          >
             <Image
               src={option.image}
               alt={option.name}
               width={500}
               height={500}
-              className="w-full h-[70px] object-cover rounded-xl"
+              className="w-full h-[70px] object-cover rounded-lg transition-transform duration-200 group-hover:scale-[1.02]"
             />
-            <h2 className="text-xs text-center">{option.name}</h2>
+            <h2 className="text-xs text-center mt-1 font-medium">
+              {option.name}
+            </h2>
           </div>
         ))}
       </div>
